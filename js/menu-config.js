4f43e17d9a5c00617e11ebd45ea6fef30a44dd73
zhu_w <15828513763@163.com>
2018-01-31 16:49:44 +0800

    登录页面修改

--- js/menu-config.js
+++ js/menu-config.js
@@ -2,7 +2,7 @@ var homeMenu = [
     {
         name: '首页',                //名称
         type: 'home',
-        url: 'html/home.html',      //路径
+        type: 'home',      //路径
         icon: 'fa-home' ,           //图标样式
         children: []                //子目录
     },
@@ -16,8 +16,8 @@ var homeMenu = [
         name: '组织机构',
         icon: 'fa-sitemap',
         children: [
-            {name: '机构设置',type: 'jgsz',url: 'jgsz'},
-            {name: '数据统计',type: 'sjtj',url: 'html/sjtj.html'}
+            {name: '机构设置',type: 'jgsz'},
+            {name: '数据统计',type: 'sjtj'}
         ]
     },
     {
@@ -25,46 +25,46 @@ var homeMenu = [
         icon: 'fa-users',
         children: [
             {name: '入党积极分子',type:'rdjjfz'},
-            {name: '发展对象信息',url: 'html/fzdxxx.html'},
-            {name: '预备党员管理',url: 'html/ybdygl.html'},
-            {name: '预备转正党员管理',url: 'html/ybzzdygl.html'}
+            {name: '发展对象信息',type: 'fzdxxx'},
+            {name: '预备党员管理',type: 'ybdygl'},
+            {name: '预备转正党员管理',type: 'ybzzdygl'}
         ]
     },
     {
         name: '党员管理',
         icon: 'fa-th',
         children: [
-            {name: '党员登记管理',url: 'html/dydjgl.html'},
-            {name: '流动党员管理',url: 'html/lddygl.html'},
-            {name: '党员注销管理',url: 'html/dyzxgl.html'},
-            {name: '党费收缴管理',url: 'html/dfsjgl.html'},
-            {name: '党员评议管理',url: 'html/dypygl.html'}
+            {name: '党员登记管理',type: 'dydjgl'},
+            {name: '流动党员管理',type: 'lddygl'},
+            {name: '党员注销管理',type: 'dyzxgl'},
+            {name: '党费收缴管理',type: 'dfsjgl'},
+            {name: '党员评议管理',type: 'dypygl'}
         ]
     },
     {
         name: '党员服务',
         icon: 'fa-tasks',
-        url: 'html/debug.html'
+        type: 'debug'
     },
     {
         name: '党员活动',
         icon: 'fa-th-large',
         children: [
-            {name: '活动开展',url: 'html/hdkz.html'},
-            {name: '活动情况统计',url: 'html/hdqktj.html'}
+            {name: '活动开展',type: 'hdkz'},
+            {name: '活动情况统计',type: 'hdqktj'}
         ]
     },
     {
         name: '居民议事',
         icon: 'fa-list-alt',
         children: [
-            {name: '议题受理情况',url: 'html/debug.html'},
-            {name: '议题审查情况',url: 'html/debug.html'},
-            {name: '会议公告信息',url: 'html/debug.html'},
-            {name: '会议通知信息',url: 'html/debug.html'},
-            {name: '表决决议公告',url: 'html/debug.html'},
-            {name: '实施结果公告',url: 'html/debug.html'},
-            {name: '实施结果季度公告',url: 'html/debug.html'},
+            {name: '议题受理情况',type: 'debug'},
+            {name: '议题审查情况',type: 'debug'},
+            {name: '会议公告信息',type: 'debug'},
+            {name: '会议通知信息',type: 'debug'},
+            {name: '表决决议公告',type: 'debug'},
+            {name: '实施结果公告',type: 'debug'},
+            {name: '实施结果季度公告',type: 'debug'},
         ]
     }
 ];
